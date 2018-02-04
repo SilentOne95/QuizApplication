258935e88a07338d9328062e682db0eb94350126
SilentOne <SilentOne@onet.pl>
2018-02-04 19:16:34 +0100

    Testing onSaveInstanceState

--- app/src/main/java/com/example/android/quizapplication/questions.java
+++ app/src/main/java/com/example/android/quizapplication/questions.java
@@ -38,6 +38,35 @@ public class questions extends AppCompatActivity {
     boolean supportOne = false;
     boolean supportTwo = false;
 
+    static final String FIRST_QUESTION_STATE = "questionOne";
+    static final String SECOND_QUESTION_STATE = "questionTwo";
+    static final String THIRD_QUESTION_STATE = "questionThree";
+    static final String FOURTH_QUESTION_STATE = "questionFour";
+    static final String FIFTH_QUESTION_STATE = "questionFive";
+    static final String SIXTH_QUESTION_STATE = "questionSix";
+    static final String SEVENTH_QUESTION_STATE = "questionSeven";
+    static final String EIGHTH_QUESTION_STATE = "questionEight";
+    static final String NINTH_QUESTION_STATE= "questionNine";
+    static final String TENTH_QUESTION_STATE = "questionTen";
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putInt(FIRST_QUESTION_STATE, firstQuestionPoints);
+        savedInstanceState.putInt(SECOND_QUESTION_STATE, secondQuestionPoints);
+        savedInstanceState.putInt(THIRD_QUESTION_STATE, thirdQuestionPoints);
+        savedInstanceState.putInt(FOURTH_QUESTION_STATE, fourthQuestionPoints);
+        savedInstanceState.putInt(FIFTH_QUESTION_STATE, fifthQuestionPoints);
+        savedInstanceState.putInt(SIXTH_QUESTION_STATE, sixthQuestionPoints);
+        savedInstanceState.putInt(SEVENTH_QUESTION_STATE, seventhQuestionPoints);
+        savedInstanceState.putInt(EIGHTH_QUESTION_STATE, eighthQuestionPoints);
+        savedInstanceState.putInt(NINTH_QUESTION_STATE, ninthQuestionPoints);
+        savedInstanceState.putInt(TENTH_QUESTION_STATE, tenthQuestionPoints);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +75,21 @@ public class questions extends AppCompatActivity {
         stringInputOne = findViewById(R.id.top_scorer_name);
         stringInputTwo = findViewById(R.id.city_name_with_clock);
         stringInputThree = findViewById(R.id.trainer_name);
+
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            firstQuestionPoints = savedInstanceState.getInt(FIRST_QUESTION_STATE);
+            secondQuestionPoints = savedInstanceState.getInt(SECOND_QUESTION_STATE);
+            thirdQuestionPoints = savedInstanceState.getInt(THIRD_QUESTION_STATE);
+            fourthQuestionPoints = savedInstanceState.getInt(FOURTH_QUESTION_STATE);
+            fifthQuestionPoints = savedInstanceState.getInt(FIFTH_QUESTION_STATE);
+            sixthQuestionPoints = savedInstanceState.getInt(SIXTH_QUESTION_STATE);
+            seventhQuestionPoints = savedInstanceState.getInt(SEVENTH_QUESTION_STATE);
+            eighthQuestionPoints = savedInstanceState.getInt(EIGHTH_QUESTION_STATE);
+            ninthQuestionPoints = savedInstanceState.getInt(NINTH_QUESTION_STATE);
+            tenthQuestionPoints = savedInstanceState.getInt(TENTH_QUESTION_STATE);
+        }
     }
 
     // all methods that check whether player scored point or not
